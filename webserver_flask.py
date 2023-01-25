69a3885952712f913ca5e0f841cb187509b6a157
NathanVrieland <nathanvrieland@gmail.com>
2023-01-25 13:46:50 -0500

    maybe made websockets work

--- webserver_flask.py
+++ webserver_flask.py
@@ -1,10 +1,12 @@
-# this does the same thing as the other two webservers, but uses python flask library
+#this does the same thing as the other two webservers, but uses python flask library
 from flask import Flask, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+host = ""
+port = 80
 
 @app.route('/')
 def handle_root():
@@ -26,11 +28,17 @@ def handle_content():
 
 @socketio.on('connect')
 def handle_connect():
-    print('Client connected')
+    print('websocket Client connected')
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    print('Client disconnected')
+    print('websocket Client disconnected')
+
+@socketio.on('message')
+def handle_message(message):
+    if message == 'update':
+        print(f"Websocket message: {message}")
+        emit('update', {'data': "nm"}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, port=80, host="")
