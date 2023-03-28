2d7ec340209a2907e11c72e263880d6935cec4bc
NathanVrieland <nathanvrieland@gmail.com>
2023-03-28 11:38:40 -0400

    dafedwer

--- webserver_flask.py
+++ webserver_flask.py
@@ -59,8 +59,9 @@ def handle_message(message):
         try:
             print(f"user {userlookup.fetchall()[0][0]} ({data['name']}) sent a message")
         except IndexError:
-            print("user not found")
+            print(f"user {data['name']} not found")
             return
+        print("success!")
         # with open("messages.txt", 'a') as messagefile:
         #     messagefile.write(f"{data['name']}: {data['message']}\n")
         # the emit's data field could potentially send back a checksum + the new message and the client could decide if it needs to get all the messages or not
