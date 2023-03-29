f90230267c3319f7bff8d2b40767fc5a11a54fac
NathanVrieland <nathanvrieland@gmail.com>
2023-03-29 13:14:13 -0400

    dafsdfdf

--- auth.py
+++ auth.py
@@ -4,7 +4,7 @@ import mysql.connector
 
 
 class Login:
-    def __init__(self, cursor, username: str, password: str):
+    def __init__(self, cursor: mysql.connector.cursor.MySQLCursor, username: str, password: str):
         self.cursor = cursor
         self.username = username
         self.key = get_key(username, password)
