7f0380f728a04e9aaf9ac9dbab3d4af23c284e7f
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-04-01 14:01:59 -0400

    hex decode

--- auth.py
+++ auth.py
@@ -51,8 +51,7 @@ class Signup(Authenticator):
         super().__init__(db, username, password)
 
     def signup(self):
-        salt = os.urandom(32)
-        print('######## GEN SALT', salt.hex().encode('utf-8'))
+        salt = os.urandom(32).hex().encode('utf-8')
         key = generate_key(self.password, salt)
         print('KEY ###########' + key, salt.hex())
         self.cursor.execute(f"INSERT INTO users (name, password, browser_cookie, salt) VALUES ('{self.username}', '{key}', '754', '{salt.hex()}')")
