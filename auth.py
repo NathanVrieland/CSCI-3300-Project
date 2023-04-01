538ac95789269d62094f09c1e72d6467d151e8a1
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-04-01 13:59:20 -0400

    s

--- auth.py
+++ auth.py
@@ -55,7 +55,7 @@ class Signup(Authenticator):
         print('######## GEN SALT', salt)
         key = generate_key(self.password, salt)
         print('KEY ###########' + key, salt.hex())
-        self.cursor.execute(f"INSERT INTO users (name, password, browser_cookie, salt) VALUES ('{self.username}', '{key}', '555555', '{salt.hex()}')")
+        self.cursor.execute(f"INSERT INTO users (name, password, browser_cookie, salt) VALUES ('{self.username}', '{key}', '6688', '{salt.hex()}')")
         self.db.commit()
         redirect('/login.html', code=302)
 
