e2f90d8041512a1aff4e53bfa311521ef8466b3c
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-04-01 13:25:40 -0400

    33

--- auth.py
+++ auth.py
@@ -54,7 +54,7 @@ class Signup(Authenticator):
         salt = os.urandom(32)
         key = generate_key(self.password, salt)
         print('KEY ###########' + key, salt.hex())
-        self.cursor.execute(f"INSERT INTO users (name, password, browser_cookie, salt) VALUES ('{self.username}', '{key}', '222222', '{salt.hex()}')")
+        self.cursor.execute(f"INSERT INTO users (name, password, browser_cookie, salt) VALUES ('{self.username}', '{key}', '33333', '{salt.hex()}')")
         self.db.commit()
         redirect('/login.html', code=302)
 
